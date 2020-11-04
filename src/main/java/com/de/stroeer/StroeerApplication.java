a49237521787975c839c39ccd9ebfe0b98bded19
avinitjsr <61912629+avinitjsr@users.noreply.github.com>
2020-11-04 17:12:15 +0100

    Add files via upload

--- src/main/java/com/de/stroeer/StroeerApplication.java
+++ src/main/java/com/de/stroeer/StroeerApplication.java
@@ -1,1 +1,13 @@
+package com.de.stroeer;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StroeerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StroeerApplication.class, args);
+	}
+
+}
