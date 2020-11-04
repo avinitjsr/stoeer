77b74270f18460154edb749ba5fdb79aa1824c3c
avinitjsr <61912629+avinitjsr@users.noreply.github.com>
2020-11-04 17:10:29 +0100

    Add files via upload

--- src/main/java/com/de/stroeer/repository/UserRepository.java
+++ src/main/java/com/de/stroeer/repository/UserRepository.java
@@ -1,1 +1,11 @@
+package com.de.stroeer.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.de.stroeer.model.Users;
+
+@Repository
+public interface UserRepository extends JpaRepository<Users, Long>{
+
+}
