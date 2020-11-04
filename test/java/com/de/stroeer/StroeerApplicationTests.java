bc86defc60a4b16056ab5847a9f0f32de33c0f31
avinitjsr <61912629+avinitjsr@users.noreply.github.com>
2020-11-04 17:17:02 +0100

    Add files via upload

--- test/java/com/de/stroeer/StroeerApplicationTests.java
+++ test/java/com/de/stroeer/StroeerApplicationTests.java
@@ -1,1 +1,13 @@
+package com.de.stroeer;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StroeerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
